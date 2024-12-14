c48d015dd54c08024fbad1509b80ad23a4ef28fe
Taufiq <taufiq9.2004@gmail.com>
2024-12-14 15:59:12 +0530

    changes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { GoogleLogin } from '@react-oauth/google'
 import Link from 'next/link'
 // import { useRouter } from 'next/router';
 import { usePathname } from 'next/navigation'
@@ -11,6 +12,15 @@ const Header = () => {
     <div className='px-7 md:px-[30%] my-4 flex'>
       {pathname == '/' ? <div className='text-pink-500'><Link href={'/'}>Second Brain</Link></div>: <div className='text-gray-500'><Link href={'/'}>Second Brain</Link></div>}
       {pathname == '/explore' ? <div className='text-pink-500'><Link href={'/explore'}>/ Explore</Link></div>:<div className='text-gray-500'><Link href={'/explore'}>/ Explore</Link></div>}
+
+      <GoogleLogin
+        onSuccess={credentialResponse => {
+          console.log(credentialResponse);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />;
       {/* <div className='text-gray-500'><span>{" / "} </span>explore /</div> */}
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <GoogleOAuthProvider clientId="813721808048-0eflq5dlubmq6e6udn81b3elfqugeldj.apps.googleusercontent.com">
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -32,5 +34,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </GoogleOAuthProvider>
   );
 }
